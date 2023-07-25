6716354c48d9b55da24f30cc9bb081424fbaa56b
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-07-24 22:35:29 -0500

    added to taco menu and created order file

--- server/models/tacos.js
+++ server/models/tacos.js
@@ -5,10 +5,18 @@ const menuSchema = new Schema (
        name: {
         type: String 
        },
+       description: {
+         type: String
+       },
        price: {
         type: Number, 
         required: true
-       }
+       },
+       image: {
+            type: String
+        },
+
+
     }
 )
 
